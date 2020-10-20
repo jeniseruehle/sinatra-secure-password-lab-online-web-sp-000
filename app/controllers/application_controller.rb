05e74bd6e9183b6b729d04b21818fdf988bc0188
Jenise Ruehle <jela07@gmail.com>
2020-10-20 16:22:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,9 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
     if user.save && params[:username] !="" && params[:password] !=""
-      redirect to "/login"
+      redirect "/login"
     else
-      redirect to "/failure"
+      redirect "/failure"
     end
   end
 
@@ -39,9 +39,9 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to "/account"
+      redirect "/account"
     else
-      redirect to "/failure"
+      redirect "/failure"
     end
   end
 
